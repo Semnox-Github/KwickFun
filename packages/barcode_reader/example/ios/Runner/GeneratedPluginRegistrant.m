968e35003dee96b048fe1031693311eeb1a98e13
shobithDS <shobith.ds@semnox.com>
2024-03-13 15:18:54 +0530

    Added barcode_reader

---
+++ packages/barcode_reader/example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<barcode_reader/BarcodeReaderPlugin.h>)
+#import <barcode_reader/BarcodeReaderPlugin.h>
+#else
+@import barcode_reader;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [BarcodeReaderPlugin registerWithRegistrar:[registry registrarForPlugin:@"BarcodeReaderPlugin"]];
+}
+
+@end
